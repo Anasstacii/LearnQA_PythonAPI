aed945300a1f45d95afc2b4b466f23025eaa3af1
anasstaccii <Anasstaccii@gmail.com>
2022-11-29 20:51:28 +0300

    ex10

--- short_text.py
+++ short_text.py
@@ -1,4 +1,4 @@
 class Test:
     def test_check_lenght(self):
-        phrase = input("Type any phrase less than 15 characters: ")
+        phrase = input("Set a phrase: ")
         assert len(phrase) < 15, f"Typed phrase more than 15 characters. You typed {len(phrase)} characters"
