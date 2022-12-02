96bc9f7db259a87a9c8cac7419f6cec78b921b9d
anasstaccii <Anasstaccii@gmail.com>
2022-12-02 20:54:24 +0300

    ex15

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,22 +1,46 @@
 import json.decoder
+import string
+import random
+from datetime import datetime
 
 from requests import Response
 
-class BaseCase:
-    def get_cookie (self, response: Response, cookie_name):
-            assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
-            return response.cookies[cookie_name]
 
-    def get_header (self, response: Response, headers_name):
-            assert headers_name in response.headers, f"Cannot find header with name {headers_name} in the last response"
-            return response.headers[headers_name]
+class BaseCase:
+    def get_cookie(self, response: Response, cookie_name):
+        assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
+        return response.cookies[cookie_name]
+
+    def get_header(self, response: Response, headers_name):
+        assert headers_name in response.headers, f"Cannot find headers with name {headers_name} in the last response"
+        return response.headers[headers_name]
 
     def get_json_value(self, response: Response, name):
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
-            assert False, f"Response is not is JSON format. Response text is '{response.text}'"
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
-        assert name in response_as_dict, f"Response JSON doesn't have key '{name}"
-
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
+
+    def generate_random_string(self, length):
+        letters = string.ascii_lowercase
+        rand_string = ''.join(random.choice(letters) for i in range(length))
+        return rand_string
+
+
---
+++ tests/userRegister.py
@@ -0,0 +1,85 @@
+import pytest
+from lib.assertions import Assertions
+import requests
+from lib.base_case import BaseCase
+
+class TestUserRegister(BaseCase):
+    data = [
+        ("email"),
+        ("password"),
+        ("username"),
+        ("firstName"),
+        ("lastName")
+    ]
+
+    def test_create_user_with_incorrect_email(self): # без символа @
+        email = 'learnqaexample.ru'
+        data = self.prepare_registration_data(email)
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == "Invalid email format", f"Unexpected response content {response.content}"
+
+
+    @pytest.mark.parametrize('condition', data) #без указания одного из полей
+    def test_user_without_filed(self, condition):
+        if condition == "email":
+            data = self.prepare_registration_data().copy()
+            data.pop(condition)
+            response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+            Assertions.assert_code_status(response, 400)
+            assert response.content.decode("utf-8") == f"The following required params are missed: {condition}", f"Unexpected response content {response.content}"
+
+        if condition == "password":
+            data = self.prepare_registration_data().copy()
+            data.pop(condition)
+            response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+            Assertions.assert_code_status(response, 400)
+            assert response.content.decode("utf-8") == f"The following required params are missed: {condition}", f"Unexpected response content {response.content}"
+
+        if condition == "username":
+            data = self.prepare_registration_data().copy()
+            data.pop(condition)
+            response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+            Assertions.assert_code_status(response, 400)
+            assert response.content.decode("utf-8") == f"The following required params are missed: {condition}", f"Unexpected response content {response.content}"
+
+        if condition == "firstName":
+            data = self.prepare_registration_data().copy()
+            data.pop(condition)
+            response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+            Assertions.assert_code_status(response, 400)
+            assert response.content.decode("utf-8") == f"The following required params are missed: {condition}", f"Unexpected response content {response.content}"
+
+        if condition == "lastName":
+            data = self.prepare_registration_data().copy()
+            data.pop(condition)
+            response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+            Assertions.assert_code_status(response, 400)
+            assert response.content.decode("utf-8") == f"The following required params are missed: {condition}", f"Unexpected response content {response.content}"
+
+
+    def test_user_short_name(self): # имя с одним символом
+        data = self.prepare_registration_data().copy()
+        short_value = 'a'
+        data.update({'firstName': short_value})
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The value of 'firstName' field is too short", f"Unexpected response content {response.content}"
+
+
+    def test_user_with_long_name(self): #имя длиннее 250 символов
+        data = self.prepare_registration_data().copy()
+        long_value = self.generate_random_string(251)
+        print(long_value)
+        data.update({'firstName': long_value})
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        print(response.content)
+        assert response.content.decode("utf-8") == f"The value of 'firstName' field is too long", f"Unexpected response content {response.content}"
